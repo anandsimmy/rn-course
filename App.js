e325bbae8282db3c950624d105306ee98aedc553
Anand Simmy <anandsimmy7@gmail.com>
2022-04-01 08:22:31 +0530

    style changes

--- App.js
+++ App.js
@@ -1,53 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        alert('Hi');
-      }}
-    >
-      <View style={styles.container}>
-        <Text
-          style={{
-            borderColor: 'red',
-            borderWidth: 1,
-            padding: 16,
-            margin: 20,
-          }}
-        >
-          Anand is the best!
-        </Text>
-        <TouchableWithoutFeedback onPress={() => alert('hello')}>
-          <View
-            style={{
-              height: 100,
-              width: 100,
-              backgroundColor: 'green',
-            }}
-          >
-            <Text style={{ color: 'white' }}>Click Me</Text>
-          </View>
-        </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <View style={styles.inputWrapper}>
+        <TextInput placeholder='Your goals' />
+        <Button title='Add your Goals' />
       </View>
-    </TouchableWithoutFeedback>
+      <View style={styles.listWrapper}>
+        <Text>List of all Goals</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 50,
+    paddingTop: 100,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
+    justifyContent: 'space-between',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+  },
+  listWrapper: {
+    paddingTop: 50,
   },
 });
