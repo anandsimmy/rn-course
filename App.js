7feefc4f1f482bd19920b7695fdc70bf5a5e2b58
Anand Simmy <anandsimmy7@gmail.com>
2022-05-19 08:41:02 +0530

    minor changes

--- App.js
+++ App.js
@@ -32,6 +32,7 @@ const App = () => {
           return (
             <Text style={styles.listItemStyles} key={index}>
               {courseGoal}
+              {index}
             </Text>
           );
         })}
