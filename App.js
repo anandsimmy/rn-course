469631c35c2a4067bd30a76b0009b640a822d801
Anand Simmy <anandsimmy7@gmail.com>
2022-06-15 07:49:03 +0530

    added modal

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ import {
   TextInput,
   FlatList,
   Pressable,
+  Modal,
 } from 'react-native';
 
 const App = () => {
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
 
   const textInputHandler = (event) => {
@@ -26,34 +28,40 @@ const App = () => {
       ];
     });
     setEnteredGoalText('');
+    setModalVisible(false);
   };
 
-  const onPressFunction = (index) => {
-    console.log('pressed', index);
+  const deleteGoalHandler = (id) => {
+    console.log('pressed', id);
+    // setCourseGoals((currentGoals) => {
+    //   currentGoals.splice(index, 1);
+    //   return [...currentGoals];
+    // });
     setCourseGoals((currentGoals) => {
-      currentGoals.splice(index, 1);
-      return [...currentGoals];
+      return currentGoals.filter((currentGoal) => {
+        return currentGoal.key !== id;
+      });
     });
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          style={styles.inputStyles}
-          placeholder='Your goals'
-          onChangeText={textInputHandler}
-          value={enteredGoalText}
-        />
-        <Button onPress={addGoalHandler} title='Add Goal' />
-      </View>
+      <Button
+        title='Add New Goal'
+        onPress={() => setModalVisible(true)}
+        color='#5e0acc'
+      />
+
       <View style={styles.listWrapper}>
         <Text>List of all Goals</Text>
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
             return (
-              <Pressable onPress={() => onPressFunction(itemData.index)}>
+              <Pressable
+                android_ripple={{ color: '#ddddd', borderless: true }}
+                onPress={() => deleteGoalHandler(itemData.item.key)}
+              >
                 <View style={styles.listItemStyles}>
                   <Text style={styles.listText}>{itemData.item.text}</Text>
                 </View>
@@ -73,23 +81,27 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 24,
-    paddingHorizontal: 24,
+    margin: 24,
+    marginTop: 48,
+  },
+  modalWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 24,
   },
   inputWrapper: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 36,
+    height: 200,
+    width: '100%',
+    justifyContent: 'space-between',
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+    margin: 8,
   },
   inputStyles: {
-    flex: 1,
     borderWidth: 1,
     borderColor: '#ccc',
     padding: 8,
-    marginRight: 8,
   },
   listWrapper: {
     flex: 4,
